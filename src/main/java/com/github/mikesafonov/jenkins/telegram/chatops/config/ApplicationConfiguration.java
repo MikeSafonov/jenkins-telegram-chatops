77e5788f040c1f286a4a504bc0363cec793e2c54
msafonov <msafonovmail@gmail.com>
2019-12-24 14:37:45 +0300

    refactor: convert JenkinsServer to bean

--- src/main/java/com/github/mikesafonov/jenkins/telegram/chatops/config/ApplicationConfiguration.java
+++ src/main/java/com/github/mikesafonov/jenkins/telegram/chatops/config/ApplicationConfiguration.java
@@ -1,16 +1,29 @@
 package com.github.mikesafonov.jenkins.telegram.chatops.config;
 
+import com.offbytwo.jenkins.JenkinsServer;
 import org.apache.http.HttpHost;
 import org.apache.http.client.config.RequestConfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.telegram.telegrambots.bots.DefaultBotOptions;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+
 /**
  * @author Mike Safonov
  */
 @Configuration
 public class ApplicationConfiguration {
+
+    @Bean
+    public JenkinsServer jenkinsServer(JenkinsInstanceProperties jenkinsInstanceProperties) throws URISyntaxException {
+        return new JenkinsServer(
+                new URI(jenkinsInstanceProperties.getUrl()),
+                jenkinsInstanceProperties.getUsername(),
+                jenkinsInstanceProperties.getToken()
+        );
+    }
 
     @Bean
     public DefaultBotOptions botOptions(TelegramBotProperties telegramBotProperties) {
--- src/main/java/com/github/mikesafonov/jenkins/telegram/chatops/jenkins/JenkinsServerWrapper.java
+++ src/main/java/com/github/mikesafonov/jenkins/telegram/chatops/jenkins/JenkinsServerWrapper.java
@@ -1,16 +1,14 @@
 package com.github.mikesafonov.jenkins.telegram.chatops.jenkins;
 
-import com.github.mikesafonov.jenkins.telegram.chatops.config.JenkinsInstanceProperties;
 import com.offbytwo.jenkins.JenkinsServer;
 import com.offbytwo.jenkins.model.FolderJob;
 import com.offbytwo.jenkins.model.Job;
 import com.offbytwo.jenkins.model.JobWithDetails;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
-import java.net.URI;
-import java.net.URISyntaxException;
 import java.util.*;
 
 /**
@@ -18,23 +16,12 @@ import java.util.*;
  */
 @Log4j2
 @Component
+@RequiredArgsConstructor
 public class JenkinsServerWrapper {
     static final String FOLDER_CLASS = "com.cloudbees.hudson.plugins.folder.Folder";
     static final String WORKFLOW_MULTIBRANCH_PROJECT_CLASS = "org.jenkinsci.plugins.workflow.multibranch.WorkflowMultiBranchProject";
 
     private final JenkinsServer jenkinsServer;
-
-    public JenkinsServerWrapper(JenkinsInstanceProperties jenkinsInstanceProperties) {
-        try {
-            jenkinsServer = new JenkinsServer(
-                    new URI(jenkinsInstanceProperties.getUrl()),
-                    jenkinsInstanceProperties.getUsername(),
-                    jenkinsInstanceProperties.getToken()
-            );
-        } catch (URISyntaxException e) {
-            throw new RuntimeException(e);
-        }
-    }
 
     public JenkinsServer getJenkinsServer() {
         return jenkinsServer;
