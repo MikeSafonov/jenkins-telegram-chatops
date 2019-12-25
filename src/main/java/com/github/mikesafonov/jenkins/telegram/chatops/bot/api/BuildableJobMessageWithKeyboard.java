ee0b78d4c16dd0202ea034479459dede3fcfd7d9
msafonov <msafonovmail@gmail.com>
2019-12-25 14:57:21 +0300

    chore: reduce duplicate code

--- src/main/java/com/github/mikesafonov/jenkins/telegram/chatops/bot/api/BuildableJobMessageWithKeyboard.java
+++ src/main/java/com/github/mikesafonov/jenkins/telegram/chatops/bot/api/BuildableJobMessageWithKeyboard.java
@@ -1,6 +1,5 @@
 package com.github.mikesafonov.jenkins.telegram.chatops.bot.api;
 
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
 
 import java.util.List;
@@ -8,27 +7,17 @@ import java.util.List;
 /**
  * @author Mike Safonov
  */
-public class BuildableJobMessageWithKeyboard extends MessageWithKeyboard {
-    private final String jobUrl;
-    private final String jobName;
+public class BuildableJobMessageWithKeyboard extends JobMessageWithKeyboard {
 
     public BuildableJobMessageWithKeyboard(Long chatId, String text, String jobName, String jobUrl) {
-        super(chatId, text);
-        this.jobUrl = jobUrl;
-        this.jobName = jobName;
-        setReplyMarkup(getInlineKeyboardMarkup());
+        super(chatId, text, jobUrl, jobName);
     }
 
     @Override
-    protected InlineKeyboardMarkup getInlineKeyboardMarkup() {
-        InlineKeyboardMarkup markupInline = new InlineKeyboardMarkup();
-
-        List<InlineKeyboardButton> buttons = List.of(
+    protected List<InlineKeyboardButton> getButtons() {
+        return List.of(
                 new InlineKeyboardButton().setText("Launch on Jenkins").setUrl(jobUrl),
                 new InlineKeyboardButton().setText("Run").setCallbackData("run=" + jobName)
         );
-
-        markupInline.setKeyboard(List.of(buttons));
-        return markupInline;
     }
 }
--- src/main/java/com/github/mikesafonov/jenkins/telegram/chatops/bot/api/FolderJobMessageWithKeyboard.java
+++ src/main/java/com/github/mikesafonov/jenkins/telegram/chatops/bot/api/FolderJobMessageWithKeyboard.java
@@ -1,6 +1,5 @@
 package com.github.mikesafonov.jenkins.telegram.chatops.bot.api;
 
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
 
 import java.util.List;
@@ -8,27 +7,16 @@ import java.util.List;
 /**
  * @author Mike Safonov
  */
-public class FolderJobMessageWithKeyboard extends MessageWithKeyboard {
-    private final String jobUrl;
-    private final String jobName;
-
+public class FolderJobMessageWithKeyboard extends JobMessageWithKeyboard {
     public FolderJobMessageWithKeyboard(Long chatId, String text, String jobName, String jobUrl) {
-        super(chatId, text);
-        this.jobUrl = jobUrl;
-        this.jobName = jobName;
-        setReplyMarkup(getInlineKeyboardMarkup());
+        super(chatId, text, jobUrl, jobName);
     }
 
     @Override
-    protected InlineKeyboardMarkup getInlineKeyboardMarkup() {
-        InlineKeyboardMarkup markupInline = new InlineKeyboardMarkup();
-
-        List<InlineKeyboardButton> buttons = List.of(
+    protected List<InlineKeyboardButton> getButtons() {
+        return List.of(
                 new InlineKeyboardButton().setText("Launch on Jenkins").setUrl(jobUrl),
                 new InlineKeyboardButton().setText("See child jobs").setCallbackData("folder=" + jobName)
         );
-
-        markupInline.setKeyboard(List.of(buttons));
-        return markupInline;
     }
 }
