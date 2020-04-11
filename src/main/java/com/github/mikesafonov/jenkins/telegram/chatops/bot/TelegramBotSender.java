71c53b56ed73356a20835cf9d5a71dec984d9e3c
msafonov <msafonovmail@gmail.com>
2020-04-11 12:17:18 +0300

    feat: added error message when unable to send telegram message

--- src/main/java/com/github/mikesafonov/jenkins/telegram/chatops/bot/TelegramBotSender.java
+++ src/main/java/com/github/mikesafonov/jenkins/telegram/chatops/bot/TelegramBotSender.java
@@ -8,6 +8,7 @@ import org.telegram.telegrambots.bots.DefaultBotOptions;
 import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
 
 import java.io.Serializable;
 
@@ -36,16 +37,28 @@ public class TelegramBotSender extends DefaultAbsSender {
     }
 
     public void sendTextMessage(Long chatId, String text) {
-        try {
-            sendApiMethod(new SendMessage(chatId, text));
-        } catch (TelegramApiException e) {
-            log.error(e.getMessage(), e);
-        }
+        sendTelegramMessage(new SendMessage(chatId, text));
     }
 
     public void sendMarkdownTextMessage(Long chatId, String text) {
+        sendTelegramMessage(new SendMessage(chatId, text).enableMarkdown(true));
+    }
+
+    private void sendTelegramMessage(SendMessage sendMessage) {
         try {
-            sendApiMethod(new SendMessage(chatId, text).enableMarkdown(true));
+            sendApiMethod(sendMessage);
+        } catch (TelegramApiRequestException e) {
+            log.error(e.toString(), e);
+            sendUnableToSendRequest(sendMessage.getChatId());
+        } catch (TelegramApiException e) {
+            log.error(e.getMessage(), e);
+            sendUnableToSendRequest(sendMessage.getChatId());
+        }
+    }
+
+    private void sendUnableToSendRequest(String chatId) {
+        try {
+            sendApiMethod(new SendMessage(chatId, "⚠️ Oops! Unable to send telegram message. Please look at logs for more information"));
         } catch (TelegramApiException e) {
             log.error(e.getMessage(), e);
         }
