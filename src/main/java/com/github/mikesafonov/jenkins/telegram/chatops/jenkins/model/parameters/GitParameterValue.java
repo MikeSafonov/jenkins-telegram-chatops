b03124a16f8c7cc146d52e19b5ce8c06615f955d
valiyvvv <valiyvvv@gmail.com>
2020-06-24 05:33:47 +0000

    feat: added GitParameter

---
+++ src/main/java/com/github/mikesafonov/jenkins/telegram/chatops/jenkins/model/parameters/GitParameterValue.java
@@ -0,0 +1,7 @@
+package com.github.mikesafonov.jenkins.telegram.chatops.jenkins.model.parameters;
+
+/**
+ * @author Mike Safonov
+ */
+public class GitParameterValue extends ParameterValue<String> {
+}
